217d98e0a1b8839b99bb3c58e8f2077b836660f1
mortyflex <mortyflex@gmail.com>
2020-05-09 05:39:52 +0200

    Updates

---
+++ precache-manifest.9f03d66a2f1e5830cd6117c68da125be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db42f3a713f2ac0326a15c8ce5fce32",
+    "url": "/cov-track/index.html"
+  },
+  {
+    "revision": "b67f6bfbed52f3ee0d52",
+    "url": "/cov-track/static/css/main.7f24ea41.chunk.css"
+  },
+  {
+    "revision": "d406c1843efe59335888",
+    "url": "/cov-track/static/js/2.6c37d457.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/cov-track/static/js/2.6c37d457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b67f6bfbed52f3ee0d52",
+    "url": "/cov-track/static/js/main.b37242b4.chunk.js"
+  },
+  {
+    "revision": "78cda426113b411a78a7",
+    "url": "/cov-track/static/js/runtime-main.fb0912ed.js"
+  },
+  {
+    "revision": "15e39f76b938cf01cd1d214fb970f633",
+    "url": "/cov-track/static/media/coronavirus.15e39f76.png"
+  }
+]);
